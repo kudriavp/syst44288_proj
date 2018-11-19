bb17ac7a071619d87cf64e7748eaec7ba3d5fde0
kudriavp <43889617+kudriavp@users.noreply.github.com>
2018-11-19 03:09:07 -0500

    removing spaces
    removing extra spaces

--- producer_consumer.c
+++ producer_consumer.c
@@ -1,85 +1,110 @@
-/*Header here*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
-
-// The internal buffer defined by a typedef consisting of a fixed-size array
 typedef int buffer_item;
-
 #define BUFFER_SIZE 5
-#define TRUE 1
 
 buffer_item buffer[BUFFER_SIZE];
-
-pthread_t thread1;
-pthread_t thread2;
-
-pthread_mutex_t mutex;
 sem_t empty;
 sem_t full;
+pthread_mutex_t mutex;
 
 int insertval = 0, removeval = 0;
 
 void *producer(void *params);
 void *consumer(void *params);
 
-/* The following code is the insert_item function which will manipulate the buffer and gets called by the producer and consumer
-   processes, respectively. */
 int insert_item(buffer_item item)
 {
-    int ret_n = 0;   
-    /* Puts semaphore 'empty' into a waiting state, waiting to see if the following if statement will return successful(0),
-       or or an error condition(-1). */
+    int ret_n;
     sem_wait(&empty);
-    // Locks the mutual-exclusion object 'mutex' 
     pthread_mutex_lock(&mutex);
     if (insertval < BUFFER_SIZE)
     {
-        buffer[insertval++] = item;
-        insertval = insertval % 5;
+        ret_n = 0;
+        buffer[insertval] = item;
+        insertval++;
+        insertval = insertval % BUFFER_SIZE;
     }
     else
     {
         ret_n = -1;
+        fprintf(stderr, "===== ERROR: Buffer Full ===== \n");
     }
-    // Unlocks the mutual-exclusion object 'mutex'
     pthread_mutex_unlock(&mutex);
-    // Increment the value of semaphore 'full' and wakes up the blocked process waiting on this one
     sem_post(&full);
-    
-    // Returns the result if adding the object to the buffer was successful or not
+
     return ret_n;
 }
 
-
-/* The following code is the remove_item function which will manipulate the buffer and gets called by the producer and consumer
-   processes, respectively. */
 int remove_item(buffer_item *item)
 {
-    int ret_n;
-    /* Puts semaphore 'full' into a waiting state, waiting to see if the following if statement will return successful(0),
-       or or an error condition(-1). */
+    int ret_n = 0;
     sem_wait(&full);
-    // Locks the mutual-exclusion object 'mutex'
+
     pthread_mutex_lock(&mutex);
-    if (insertval > 0) 
-    {
+    if (removeval < BUFFER_SIZE && removeval >= 0)
+    {   
+        ret_n = 0;
         *item = buffer[removeval];
-        buffer[removeval++] = -1;
-        removeval = removeval % 5;
+        buffer[removeval] = 0;
+        removeval++;
+        removeval = removeval % BUFFER_SIZE;
     } 
-    else 
+    else
     {
         ret_n = -1;
+        fprintf(stderr, "===== ERROR: Buffer Empty ===== \n");
     }
-    // Unlocks the mutual-exclusion object 'mutex'
     pthread_mutex_unlock(&mutex);
-    // Increment the value of semaphore 'empty' and wakes up the blocked process waiting on this one
     sem_post(&empty);
-    // Returns the result if removing the object from the buffer was successful or not
+
     return ret_n;
+}
+
+void *producer(void *param)
+{
+    buffer_item item;
+    int rn;
+
+    while(1)
+    {
+        rn = rand() % 4;
+        sleep(rn);
+        item = rand();
+
+        if(insert_item(item))
+        {
+            fprintf(stderr, "Producer error\n");
+        }
+        else
+        {
+            printf("Producer produced %d\n", item);
+        }
+    }
+
+}
+
+void *consumer(void *param)
+{
+    buffer_item item;
+    int rn;
+
+    while(1)
+    {
+        rn = rand() % 4;
+        sleep(rn);
+
+        if(remove_item(&item))
+        {
+            fprintf(stderr, "Consumer error\n");
+        }
+        else
+        {
+            printf("Consumer consumed %d \n", item);
+        }
+    }
 }
 
 int main(int argc, char *argv[])
@@ -99,7 +124,7 @@ int main(int argc, char *argv[])
 
     /* Initialize the synchronization tools */
     pthread_mutex_init(&mutex, NULL);
-    sem_init(&empty, 0, 5);
+    sem_init(&empty, 0, BUFFER_SIZE);
     sem_init(&full, 0, 0);
     srand(time(0));
 
@@ -123,50 +148,4 @@ int main(int argc, char *argv[])
     /* Sleep for user specified time */
     sleep(runtime);
     return 0;
-}
-
-//The following code is the producer process 
-void *producer(void *param)
-{
-    buffer_item item;
-    int rn;
-
-    while(1)
-    {
-        rn = rand() % 5;
-        sleep(rn);
-        item = rand();
-
-        if(insert_item(item))
-        {
-            fprintf(stderr, "Producer error\n");
-        }
-        else
-        {
-            printf("Producer produced %d\n", item);
-        }
-    }
-
-}
-
-// The following code is the consumer process
-void *consumer(void *param)
-{
-    buffer_item item;
-    int rn;
-
-    while(1)
-    {
-        rn = rand() % 5;
-        sleep(rn);
-
-        if(remove_item(&item))
-        {
-            fprintf(stderr, "Consumer error\n");
-        }
-        else
-        {
-            printf("Consumer consumed %d \n", item);
-        }
-    }
 }
