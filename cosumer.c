1a8edd0ab6eebe1a17b90f4ae4724448b5ba4348
HeritK <43890331+HeritK@users.noreply.github.com>
2018-11-18 22:12:54 -0500

    Update cosumer.c

--- cosumer.c
+++ cosumer.c
@@ -4,3 +4,29 @@
 #include <buffer.h>
 typedef int buffer item;
 #define BUFFER SIZE 5;
+void *producer(void *param) {
+ buffer item item;
+
+ while (true) {
+ /* sleep for a random period of time */
+ sleep(...);
+ /* generate a random number */
+ item = rand();
+ if (insert item(item))
+ fprintf("report error condition");
+ else
+ printf("producer produced %d\n",item);
+ }
+}
+void *consumer(void *param) {
+ buffer item item;
+
+ while (true) {
+ /* sleep for a random period of time */
+ sleep(...);
+ if (remove item(&item))
+ fprintf("report error condition");
+ else
+ printf("consumer consumed %d\n",item);
+ }
+}
