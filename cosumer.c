a495185010283f5b2f3594530f3bd47921fc498a
HeritK <43890331+HeritK@users.noreply.github.com>
2018-11-18 23:18:16 -0500

    Update cosumer.c

--- cosumer.c
+++ cosumer.c
@@ -6,6 +6,7 @@
 typedef int buffer_item;
 #define BUFFER_SIZE 5;
 
+buffer_item buffer[BUFFER_SIZE];
 
 sem_t s_empty;
 sem_t s_full;
@@ -17,7 +18,7 @@ int insertItem(buffer_item item)
 /*insert item into the buffer*/
 {
     int x=0;
-    sem_wait(&empty);
+    sem_wait(&s_empty);
 }
 
 
